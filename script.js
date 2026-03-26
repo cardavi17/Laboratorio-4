f51128c785c4ded36fd2e0f72c711c386acaee27
carlos <cardavi1700@gmail.com>
2026-03-26 15:57:32 -0600

    trabajo finalizado

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+
+//obtiene la constante de el nombre 
 const form = document.getElementById("nombre");
+//inicializa la constante de saludo con un mensaje
 const saludo = document.getElementById("saludo");
 
+//cuando se le da el boton de enviar obtienes los datos
 form.addEventListener("submit", function(event) {
     event.preventDefault();
-
+    // inicializa la variable con el con el nombre puesto
     const nombre = document.getElementById("name").value;
-
+    //concatena el nombre con el saludo
     saludo.textContent = "¡Hola " + nombre + "!";
 });
